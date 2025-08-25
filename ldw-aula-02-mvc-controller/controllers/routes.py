89decb14d85586047ef81b8a74301bfed2793e7a
DSM4 <fatec-dsm4@academico.intra>
2025-08-25 20:23:55 -0300

    :hammer:feat: Aula 2 metade

--- ldw-aula-02-mvc-controller/controllers/routes.py
+++ ldw-aula-02-mvc-controller/controllers/routes.py
@@ -1,24 +1,34 @@
-from flask import render_template
+from flask import render_template, request
 
 
 def init_app(app):
+    # Lista em Python(array)
+    players = ['Yan', 'Ferrari', 'Valéria', 'Amanda']
     @app.route('/')
-    # Definindo a rota principal da aplicação '/'
+    # Definindo a rota principal da aplicação '/
+
     def home(): # Função que será executada ao acessar a rota
         return render_template('index.html')
 
 
-    @app.route('/games')
-    def games():
+    @app.route('/games', methods=['GET', 'POST'])
+    def games(): #Essa é uma view function
         title = 'Tarisland'
         year = 2022
         category = 'MMORPG'
-        # Lista em Python(array)
-        players = ['Yan', 'Ferrari', 'Valéria', 'Amanda']
+    
         #Dicionário (Objeto)
         console = {'Nome': 'Playstation 5', 
                 'Fabricante': 'Sony',
                 'Ano': 2020}
+        
+        # Tratando uma requisição POST com request
+        if request.method == 'POST':
+            # Coletando o texto da input
+            if request.form.get('player'):
+                players.append(request.form.get('player'))
+        
+        
         return render_template('games.html',
                             title=title,
                             year=year,
